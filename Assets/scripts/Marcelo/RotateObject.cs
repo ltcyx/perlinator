588c1aabec99ae1b74f601fab82ae3b2e2812a80
marcelosabadini <sabadini69@gmail.com>
2014-07-31 21:12:02 -0300

    Added Marcelo Stuff

---
+++ Assets/scripts/Marcelo/RotateObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateObject : MonoBehaviour 
+{
+	public Vector3 rotationSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		this.gameObject.transform.Rotate(rotationSpeed);
+	}
+}
